6da4a474521ecef8b6920518d674fc34435827ac
nvchernov <kolchernov@gmail.com>
2015-10-05 20:04:52 +0300

    Исправлена верстка для стран, регионов, городов

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -28,7 +28,7 @@ class CountriesController < ApplicationController
 
     respond_to do |format|
       if @country.save
-        format.html { redirect_to @country, notice: 'Country was successfully created.' }
+        format.html { redirect_to @country, notice: 'Страна успешно создана.' }
         format.json { render :show, status: :created, location: @country }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class CountriesController < ApplicationController
   def update
     respond_to do |format|
       if @country.update(country_params)
-        format.html { redirect_to @country, notice: 'Country was successfully updated.' }
+        format.html { redirect_to @country, notice: 'Страна успешно обновлена!' }
         format.json { render :show, status: :ok, location: @country }
       else
         format.html { render :edit }
